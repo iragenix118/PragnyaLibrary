5e3d9c2340c1e93c13dc8dbbbfe3c1a083bd17f6
iragenix118 <iragenix@gmail.com>
2023-08-16 17:12:20 +0530

    rra

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                &copy; Pragnya College of Management & Computer Studies |<a href="https://Iragenix.in" target="_blank"  > Designed & Developed by :  Raj Shaikh : Iragenix </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
